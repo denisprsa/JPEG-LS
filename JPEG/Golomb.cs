afc2899009e6549a0427d5dd9b04d9cdcc99be96
denis <denis@DESKTOP-UPT1VIQ>
2016-12-21 13:14:01 +0100

    Add project files.

---
+++ JPEG/Golomb.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEG
+{
+    class Golomb
+    {
+        /**
+         *  KODIRANJE
+         * 
+         */
+        public byte[] Encode(byte[] data, int M)
+        {
+            List<byte> _out = new List<byte>();
+            for(int i = 0; i < data.Length; i++)
+            {
+                // CALCULATE Q
+                int q = data[i] / M;
+
+                for (int j = 0; j < q; j++)
+                {
+                    _out.Add(1);
+                }
+                _out.Add(0);
+
+                // CALCULATE V 
+                int v = 1;
+                for (int j = 0; j < Math.Log(M,2); j++)
+                {
+                    _out.Add((byte)(v & data[i]));
+                    v = v << 1;
+                }
+            }
+            return _out.ToArray();
+        }
+
+
+        /**
+         * 
+         *  DEKODIRANJE
+         * 
+         */
+        public void Decode(byte[] data, int M)
+        {
+            int q = 0;
+            int nr = 0;
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                q = 0;
+                nr = 0;
+
+
+            }
+        }
+    }
+}
---
+++ JPEG/JPEG.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JPEG
+{
+    public class JPEG
+    {
+        private int NEAR;
+        private int MAXVAL;
+        private int RANGE;
+        private int[] N;
+        private int[] A;
+        private int[] B;
+        private int[] C;
+        private int RUNindex;
+        private int[] J;
+        private int qbpp;
+        private int bpp;
+        private int LIMIT;
+
+        /**
+         *  NAPOLNIMO ARRAY Z DOLOCENIMI VREDNOSTMI
+         * 
+         */
+        private void Populate(this int[] arr, int value)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = value;
+            }
+        }
+
+        public void init()
+        {
+            this.N = new int[367];
+            this.A = new int[367];
+            this.B = new int[365];
+            this.C = new int[365];
+            this.Populate(this.N, 1);
+            this.Populate(this.B, 0);
+            this.Populate(this.C, 0);
+            this.RUNindex = 0;
+            this.J = new int[] {
+                0, 0, 0, 0,
+                1, 1, 1, 1,
+                2, 2, 2, 2,
+                3, 3, 3, 3,
+                4, 4, 5, 5,
+                6, 6, 7, 7,
+                8, 9,10,11,
+                12,13,14,15
+            };
+            this.RANGE = 256;
+            this.MAXVAL = 255;
+            this.qbpp = (int) Math.Log(this.RANGE, 2);
+            this.bpp = 8; // max(2, 8) -> nas primer
+        }
+
+
+        private void RunModeProcessing()
+        {
+
+        }
+
+        private void RegularModeProcessing()
+        {
+
+        }
+
+        private void GetNextSample()
+        {
+
+        }
+
+        private void Quantize(int i)
+        {
+
+        }
+
+        private void ModRange(int i)
+        {
+
+        }
+
+        private void ComputeRx()
+        {
+
+        }
+    }
+}
