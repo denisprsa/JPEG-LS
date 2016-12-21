0b7b571397799af3d91d26a91d396b09fcda8449
Denis Prša <Denis Pra>
2016-12-21 21:18:59 +0100

    added function to calculate variables for context modeling

--- JPEG/JPEG.cs
+++ JPEG/JPEG.cs
@@ -31,10 +31,23 @@ namespace JPEG
         private int c;
         private int d;
         private int x;
+
         private int D1;
         private int D2;
         private int D3;
-        
+        private int T1;
+        private int T2;
+        private int T3;
+
+        // IMAGE
+        private int posX = 0;
+        private int posY = 0;
+        private Bitmap image;
+        private int layers;
+        private int width;
+        private int height;
+        private byte[] LD;
+
 
         /**
          *  NAPOLNIMO ARRAY Z DOLOCENIMI VREDNOSTMI
@@ -54,6 +67,8 @@ namespace JPEG
          */
         private void init()
         {
+            this.C_MAX = 127;
+            this.C_MIN = -128;
             this.N = new int[367];
             this.A = new int[367];
             this.B = new int[365];
@@ -82,6 +97,8 @@ namespace JPEG
             this.qbpp = (int)Math.Log(this.RANGE, 2);
             // max(2, 8) -> nas primer
             this.bpp = 8;
+
+            this.GetNextSample();
         }
 
         /**
@@ -93,12 +110,17 @@ namespace JPEG
          */
         public byte[] Encode(string image, int n = 0)
         {
-            Bitmap file = new Bitmap(image);
+            this.image = new Bitmap(image);
             this.init();
-            int x = 0;
-            int y = 0;
 
-
+            if (this.D1 == 0 && this.D2 == 0 && this.D3 == 0)
+            {
+                this.RunModeProcessing();
+            }
+            else
+            {
+                this.RegularModeProcessing();
+            }
 
             return new byte[1];
         }
@@ -115,14 +137,66 @@ namespace JPEG
             return (i < j) ? j : i;
         }
 
+        /**
+         *  RETURN MIN VALUE BETWEEN TWO VALUES
+         * 
+         *  @i      : int
+         *  @j      : int
+         *  @return : int
+         */
         private int min(int i, int j)
         {
             return (i < j) ? i : j;
         }
 
+        /**
+         *  RUN MODE
+         * 
+         * 
+         */
         private void RunModeProcessing()
         {
+            
+        }
 
+        /**
+         * 
+         * 
+         * 
+         */
+        private void SetVariablesABCD()
+        {
+            // SET a
+            if (posX == 0 && posY == 0)
+                this.a = 0;
+            else if (posY > 0 && posX == 0)
+                this.a = this.LD[(posY - 1) * width];
+            else if (posY == 0 && posX > 0)
+                this.a = this.LD[posX - 1];
+            else
+                this.a = this.LD[((posY - 1) * width) + posX - 1];
+
+            // SET b
+            if (posY == 0)
+                this.b = 0;
+            else
+                this.b = this.LD[((posY - 1) * width) + posX];
+
+            // SET c
+            if (posY == 0 || (posY == 1 && posX == 0))
+                this.c = 0;
+            else if (posY > 1 && posX == 0)
+                this.c = this.LD[(posY - 2) * width];
+            else
+                this.c = this.LD[((posY - 1) * width) + posX - 1];
+
+            // SET d
+            if (posY == 0)
+                this.d = 0;
+            else if (posY > 0 && posX == width - 1)
+                this.d = this.LD[(posY - 1) * width - 1];
+            else
+                this.d = this.LD[((posY - 1) * width) + posX + 1];
         }
 
         private void RegularModeProcessing()
@@ -132,7 +206,7 @@ namespace JPEG
 
         private void GetNextSample()
         {
-
+            
         }
 
         private void Quantize(int i)
