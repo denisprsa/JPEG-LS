4da8538d164dea0776f0c5ab065e88f215b0a550
Denis Prša <Denis Pra>
2016-12-21 18:23:55 +0100

    added project

--- JPEG/JPEG.cs
+++ JPEG/JPEG.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,29 +12,47 @@ namespace JPEG
         private int NEAR;
         private int MAXVAL;
         private int RANGE;
+        private int qbpp;
+        private int bpp;
+        private int LIMIT;
+        private int RUNindex;
+        private int N_MAX;
+        private int C_MIN;
+        private int C_MAX;
+
         private int[] N;
         private int[] A;
         private int[] B;
         private int[] C;
-        private int RUNindex;
         private int[] J;
-        private int qbpp;
-        private int bpp;
-        private int LIMIT;
+
+        private int a;
+        private int b;
+        private int c;
+        private int d;
+        private int x;
+        private int D1;
+        private int D2;
+        private int D3;
+        
 
         /**
          *  NAPOLNIMO ARRAY Z DOLOCENIMI VREDNOSTMI
          * 
+         * 
          */
-        private void Populate(this int[] arr, int value)
+        private void Populate(int[] arr, int value)
         {
             for (int i = 0; i < arr.Length; i++)
-            {
                 arr[i] = value;
-            }
         }
 
-        public void init()
+        /**
+         *  INIT DATA
+         * 
+         * 
+         */
+        private void init()
         {
             this.N = new int[367];
             this.A = new int[367];
@@ -42,6 +61,10 @@ namespace JPEG
             this.Populate(this.N, 1);
             this.Populate(this.B, 0);
             this.Populate(this.C, 0);
+            //  PO ENCABI DOBIMO 4
+            this.Populate(this.A, 4);
+            this.N[365] = 0;
+            this.N[366] = 0;
             this.RUNindex = 0;
             this.J = new int[] {
                 0, 0, 0, 0,
@@ -50,15 +73,52 @@ namespace JPEG
                 3, 3, 3, 3,
                 4, 4, 5, 5,
                 6, 6, 7, 7,
-                8, 9,10,11,
-                12,13,14,15
+                8, 9, 10, 11,
+                12, 13, 14, 15
             };
             this.RANGE = 256;
             this.MAXVAL = 255;
-            this.qbpp = (int) Math.Log(this.RANGE, 2);
-            this.bpp = 8; // max(2, 8) -> nas primer
+            this.LIMIT = 2 * (bpp + bpp);
+            this.qbpp = (int)Math.Log(this.RANGE, 2);
+            // max(2, 8) -> nas primer
+            this.bpp = 8;
         }
 
+        /**
+         *  JPEG-LS ENCODER
+         *  
+         *  @image  : string
+         *  @n      : int
+         *  @return : byte[]
+         */
+        public byte[] Encode(string image, int n = 0)
+        {
+            Bitmap file = new Bitmap(image);
+            this.init();
+            int x = 0;
+            int y = 0;
+
+
+
+            return new byte[1];
+        }
+
+        /**
+         *  RETURN MAX VALUE BETWEEN TWO VALUES
+         * 
+         *  @i      : int
+         *  @j      : int
+         *  @return : int
+         */
+        private int max(int i, int j)
+        {
+            return (i < j) ? j : i;
+        }
+
+        private int min(int i, int j)
+        {
+            return (i < j) ? i : j;
+        }
 
         private void RunModeProcessing()
         {
--- JPEG/Program.cs
+++ JPEG/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,40 @@ namespace JPEG
     {
         static void Main(string[] args)
         {
+            if (args.Length > 2)
+            {
+                if (args[0] == "-E" && args.Length == 4)
+                {
+                    JPEG jpg = new JPEG();
+                    byte[] encodedData = jpg.Encode(args[2], Convert.ToInt32(args[1].Substring(1, args[1].Length - 1)));
 
+                    BinaryWriter write = new BinaryWriter(File.Open(args[3], FileMode.OpenOrCreate));
+                    write.Write(encodedData);
+                    write.Close();
+                }
+                else if (args[0] == "-D")
+                {
+                    JPEG jpg = new JPEG();
+                    long length = new FileInfo(args[1]).Length;
+                    BinaryReader reader = new BinaryReader(File.Open(args[1], FileMode.Open));
+                    byte[] data = new byte[length];
+                    reader.Read(data, 0, Convert.ToInt32(length));
 
+                    //jpg.DecompressImage(data, args[2]);
+                }
+                else
+                {
+                    Console.WriteLine("Check if arguments are ok.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Check if arguments are set.");
+            }
+
+            //Console.ReadKey();
         }
+
+    }
     }
 }
